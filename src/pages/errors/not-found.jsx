7466a9fcd7df9fadcbcacb88ecaa71b904b359d6
Rahul Khimsuriya <rahulkhimsuriya38@gmail.com>
2024-05-05 16:35:39 +0530

    tweak error component

--- src/pages/errors/not-found.jsx
+++ src/pages/errors/not-found.jsx
@@ -1,8 +1,18 @@
-import { useToast } from '@chakra-ui/react'
-import { useRouteError } from 'react-router-dom'
+import {
+  Box,
+  Button,
+  Center,
+  Flex,
+  Image,
+  Text,
+  useToast,
+} from '@chakra-ui/react'
+import { Link, useRouteError } from 'react-router-dom'
+
+import viteLogo from '/vite.svg'
+import { HOME_ROUTE } from '../../hooks/auth'
 
 export default function NotFound() {
-  const toast = useToast()
   const error = useRouteError()
 
   /**
@@ -11,15 +21,38 @@ export default function NotFound() {
    */
   if (import.meta.env.DEV && import.meta.env.MODE === 'development') {
     console.log('[ERRORS ROUTES]', error)
-
-    toast({
-      title: 'Errors',
-      description: JSON.stringify(error),
-      status: 'danger',
-      duration: 5000,
-      isClosable: true,
-    })
   }
 
-  return <div>NotFound</div>
+  return (
+    <Box>
+      <Flex
+        justifyContent="center"
+        alignItems="center"
+        style={{ minHeight: '100vh' }}
+      >
+        <Center>
+          <Box textAlign="center">
+            <Box display="flex" justifyContent="center">
+              <Image
+                src={viteLogo}
+                className="logo"
+                alt="Vite logo"
+                height="100px"
+              />
+            </Box>
+
+            <Box marginTop={4}>
+              <Text>Page not found</Text>
+
+              <Box as="div" marginTop={4}>
+                <Link to={HOME_ROUTE}>
+                  <Button type="submit">Back to home</Button>
+                </Link>
+              </Box>
+            </Box>
+          </Box>
+        </Center>
+      </Flex>
+    </Box>
+  )
 }
